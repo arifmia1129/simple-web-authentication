2e192ea53ced6e75dcc21188dc326d195e8511a1
arifvtti <arif.vtti@gmail.com>
2022-11-25 07:31:36 +0600

    404 and error handler route added

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/./views/index.html");
 })
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    })
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "Something broken here"
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
