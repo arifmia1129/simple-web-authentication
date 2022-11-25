140a2f08a9ef941d92b21a6df27389bff110a4ab
arifvtti <arif.vtti@gmail.com>
2022-11-25 07:53:17 +0600

    log in route created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,3 +17,37 @@ exports.createUser = async (req, res) => {
         })
     }
 }
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(404).json({
+                success: false,
+                message: "Provide email and password"
+            })
+        }
+
+        const user = await User.findOne({ $and: [{ email }, { password }] });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Email or password is invalid"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in user",
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something broken here",
+            error: error.message
+        })
+    }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller.js");
 
 router.post("/register", userController.createUser);
+router.post("/login", userController.loginUser);
 
 module.exports = router;
