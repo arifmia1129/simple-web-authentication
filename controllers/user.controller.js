f5db6370ea25160511f85f820033a130b5dbf19e
arifvtti <arif.vtti@gmail.com>
2022-11-26 08:26:59 +0600

    authentication with bcrypto

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,18 +1,32 @@
 const User = require("../models/user.model")
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
 exports.createUser = async (req, res) => {
     try {
-        const user = new User({
-            email: req.body.email,
-            password: md5(req.body.password)
+
+        bcrypt.hash(req.body.password, salt, async function (err, hash) {
+            if (err) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Something broken",
+                    error: err?.message
+                })
+            }
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            await user.save();
+            res.status(201).json({
+                success: true,
+                message: "Create the user",
+                user
+            })
         });
-        await user.save();
-        res.status(201).json({
-            success: true,
-            message: "Create the user",
-            user
-        })
+
+
     } catch (error) {
         res.status(500).json({
             success: false,
@@ -33,7 +47,9 @@ exports.loginUser = async (req, res) => {
             })
         }
 
-        const user = await User.findOne({ $and: [{ email }, { password: md5(password) }] });
+
+
+        const user = await User.findOne({ email });
 
         if (!user) {
             return res.status(404).json({
@@ -42,11 +58,22 @@ exports.loginUser = async (req, res) => {
             })
         }
 
-        res.status(200).json({
-            success: true,
-            message: "Logged in user",
-            user
-        })
+        bcrypt.compare(password, user?.password, function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Something broken",
+                    error: err?.message
+                })
+            }
+            res.status(200).json({
+                success: true,
+                message: "Logged in user",
+                user
+            })
+        });
+
+
     } catch (error) {
         res.status(500).json({
             success: false,
